464a0ec2ab9ea2af129a40c396ad78675f44432e
Grant Smith <grantnsmith@gmail.com>
2020-05-31 09:45:00 -0700

    Added burger model in burger.js.

--- config/orm.js
+++ config/orm.js
@@ -1,22 +1,22 @@
 var connection = require("../config/connection.js");
 
 var orm = {
-    selectAll: function() {
-        var queryString = "SELECT burgers FROM burger_db";
+    selectAll: function(table, cb) {
+        var queryString = "SELECT * FROM" + table + ";";
         connection.query(queryString, function(err, result) {
             if (err) throw err;
-            console.log(result);
+            cb(result);
         });
     },
-    insertOne: function(burger_name, devoured) {
-        var queryString = "INSERT INTO burgers (burger_name, devoured) VALUES (?,?)";
+    insertOne: function(table, burger_name, devoured, cb) {
+        var queryString = "INSERT INTO" + table + "(burger_name, devoured) VALUES (?,?)";
         connection.query(queryString, [burger_name, devoured], function(err, result) {
             if (err) throw err;
-            console.log(result);
+            cb(result);
         });
     },
-    updateOne: function(burger_name, devoured, id) {
-        var queryString = "UPDATE burgers SET burger_name = ?, devoured = ? WHERE id = ?";
+    updateOne: function(table, burger_name, devoured, id) {
+        var queryString = "UPDATE" + table + "SET burger_name = ?, devoured = ? WHERE id = ?";
         connection.query(queryString, [burger_name, devoured, id], function(err, result) {
             if (err) throw err;
             console.log(result);
--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,21 @@
 var orm = require("../config/orm.js");
 
-
-
-
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(burger_name, devoured, cb) {
+        orm.insertOne("burgers", burger_name, devoured, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(burger_name, devoured, id, cb) {
+        orm.updateOne("burgers", burger_name, devoured, id, function(res) {
+            cb(res);
+        })
+    }
+}
 
 module.exports = burger;
